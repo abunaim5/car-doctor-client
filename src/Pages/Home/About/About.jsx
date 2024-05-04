707a8d2c3360de7cf17b22209c8b9a4b5018933a
abunaim5 <abunaim140@gmail.com>
2024-05-04 19:17:12 +0600

    added our services component and load data and display the data

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
                 <div className="lg:w-1/2 space-y-5 p-4">
                     <h3 className='text-3xl font-semibold text-orange-500'>About Us</h3>
                     <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
-                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable. </p>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable. </p>
                     <button className="btn btn-error">Get More Info</button>
                 </div>
             </div>
